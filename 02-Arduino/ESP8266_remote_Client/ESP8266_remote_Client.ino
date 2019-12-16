93136f58698c3275b8293f936f9a61af5eacdf1d
Anas Syed <anas.syed@students.ffhs.ch>
2019-12-16 10:35:51 +0100

    add Powerbutton

--- 02-Arduino/ESP8266_remote_Client/ESP8266_remote_Client.ino
+++ 02-Arduino/ESP8266_remote_Client/ESP8266_remote_Client.ino
@@ -2,18 +2,21 @@
 #include <ArduinoHttpClient.h>
 #include "secrets.h"
 
-int potiAnalogPin = A0;
+const int potiAnalogPin = A0;
 int newVolume = 0;
 int old_volume;
-
+const int powerLedPin = 5;     
+const int PowerButtonPin = 4;  
+int powerState; 
 void setup() {
-  
   Serial.begin(115200);
+  setPin();
   Serial.print("\nConnecting to network ");
   Serial.println(SECRET_SSID);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(SECRET_SSID, SECRET_PASS);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(100); // keeps watchdog happy
   }
@@ -22,17 +25,37 @@ void setup() {
   Serial.println(WiFi.macAddress());
 }
 
+void setPin() {
+  pinMode(PowerButtonPin, INPUT);
+  pinMode(potiAnalogPin, INPUT);
+  pinMode(powerLedPin, OUTPUT);
+  digitalWrite(powerLedPin, LOW);
+  
+  }
+
 void loop() {
-  // map the Analog output to 100-0 
-  newVolume = map(analogRead(potiAnalogPin), 0, 1023, 100, 0);
+  int v = map(analogRead(potiAnalogPin), 0, 1023, 1000, 0);
+  newVolume = v / 10;
+  powerState = digitalRead(PowerButtonPin);
+
   // Change Volume if Value is +- 2 (prevent toggle) 
   if (newVolume >= (old_volume+2) || newVolume <= (old_volume-2)){
       changeVolume(newVolume);
    }
+   delay(20);
+
+  if (powerState == LOW) {
+    digitalWrite(powerLedPin, HIGH);
+    delay(200);
+    } else {
+          digitalWrite(powerLedPin, LOW);
+      }
   }
 
 void changeVolume(int volume) {
   String fullpath = path +String(volume);
+  Serial.print("fullpath: ");
+  Serial.println(fullpath);
   old_volume = volume;  
   WiFiClient wifi;
   HttpClient client = HttpClient(wifi, host, port);
@@ -44,5 +67,4 @@ void changeVolume(int volume) {
   Serial.println(statusCode);
   Serial.print("Response: ");
   Serial.println(response);
-  delay(100);
   }
