bfbae3d7147ba70f2ee28431ee87979424f2f90c
Anas Syed <anas.syed@students.ffhs.ch>
2019-12-22 16:33:42 +0100

    rm configs from secret file

--- 02-Arduino/ESP8266_remote_Client/.secrets.h
+++ 02-Arduino/ESP8266_remote_Client/.secrets.h
@@ -4,7 +4,4 @@
 
 #define SECRET_SSID "MySSID"		// replace MySSID with your WiFi network name
 #define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
-#define host "10.10.10.10" // replace Sonos Server IP
-#define port 8080 // replace Sonos Server port
-#define path "" // replace Sonos Server path
 
