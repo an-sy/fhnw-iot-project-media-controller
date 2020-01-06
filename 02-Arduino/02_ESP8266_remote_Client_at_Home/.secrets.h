96d2cf56b1e364587bca0be87717b1994178b21c
Ravindra Asirvatham <ravindra.asirvatham@students.fhnw.ch>
2020-01-06 10:43:30 +0100

    arduino version at home

---
+++ 02-Arduino/02_ESP8266_remote_Client_at_Home/.secrets.h
@@ -0,0 +1,6 @@
+  
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "TODO"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "TODO"	// replace MyPassword with your WiFi password
